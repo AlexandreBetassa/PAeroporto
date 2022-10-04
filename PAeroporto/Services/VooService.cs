8772374b63f380775d3056ad6d96564a98cc61dd
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-04 13:32:38 -0300

    inserido metodos voo Service

--- PAeroporto/Services/VooService.cs
+++ PAeroporto/Services/VooService.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace Services
 {
     internal class VooService
     {
+
+        static void Insert(Voo voo)
+        {
+
+        }
+
+        static void Select(Voo voo)
+        {
+
+        }
+        static void Update(Voo voo)
+        {
+
+        }
+        static void Delete(Voo voo)
+        {
+
+        }
+
     }
 }
