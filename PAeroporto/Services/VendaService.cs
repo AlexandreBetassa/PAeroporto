65c794a2025847c65bfa801590e3cbd45254e1c8
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-10-06 16:06:03 -0300

    concluido classe venda service

--- PAeroporto/Services/VendaService.cs
+++ PAeroporto/Services/VendaService.cs
@@ -26,46 +26,47 @@ namespace Services
 
             return venda;
         }
-        public static List<Venda> Select(Venda venda)
+        public static List<Venda> Select(int id)
         {
             List<Venda> vendaList = new();
-            
-            string select = $"SELECT cpf, nome, dataNasc, sexo, ultimaCompra, dataCad, situacao FROM dbo.passageiro WHERE cpf = @cpf";
+            string select = $"SELECT id, dataVenda, passageiroCpf, valorTotal FROM dbo.venda WHERE id = @id";
             SqlCommand sql_select = new()
             {
                 CommandText = select,
                 Connection = DataBase.OpenConnection()
             };
-            sql_select.Parameters.Add(new SqlParameter("@cpf", cpf));
+            sql_select.Parameters.Add(new SqlParameter("@id", id));
             SqlDataReader r = sql_select.ExecuteReader();
             {
                 while (r.Read())
                 {
-                    Passageiro passageiro = new()
+                    Venda venda = new()
                     {
-                        Nome = r.GetString(1),
-                        CPF = r.GetString(0),
-                        DataNascimento = r.GetDateTime(2),
-                        Sexo = r.GetString(3),
-                        UltimaCompra = r.GetDateTime(4),
-                        DataCadastro = r.GetDateTime(5),
-                        Situacao = r.GetString(6)
+                        IdVenda = r.GetInt32(0),
+                        DataVenda = r.GetDateTime(1),
+                        Passageiro = r.GetString(2),
+                        ValorTotal = r.GetFloat(3)
                     };
-                    list.Add(passageiro);
+                    vendaList.Add(venda);
                 }
             }
             DataBase.CloseConnection(sql_select.Connection);
             return vendaList;
-
-
         }
         public static void Update(Venda venda)
         {
-
+            string update = $"UPDATE dbo.venda SET dataVenda = {new SqlParameter("@dataVenda", venda.DataVenda)}, passageiroCpf = {new SqlParameter("@cpf", venda.Passageiro)}" +
+                $"valorTotal = {new SqlParameter("@valor", venda.ValorTotal)} WHERE id = {new SqlParameter("@id", venda.IdVenda)}";
+            SqlCommand sql_update = new() { CommandText = update, Connection = DataBase.OpenConnection() };
+            sql_update.ExecuteNonQuery();
+            DataBase.CloseConnection(sql_update.Connection);
         }
         public static void Delete(Venda venda)
         {
-
+            string delete = $"DELETE FROM dbo.venda WHERE id = {new SqlParameter("@id", venda.IdVenda)}";
+            SqlCommand sql_delete = new() { CommandText = delete, Connection = DataBase.OpenConnection() };
+            sql_delete.ExecuteNonQuery();
+            DataBase.CloseConnection(sql_delete.Connection);
         }
 
     }
